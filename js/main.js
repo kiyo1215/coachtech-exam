1d00936a4dde559d5d4991324a41d349915f68ca
kiyo1215 <g.k.n15@icloud.com>
2021-10-12 06:14:36 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const target = document.getElementById("hamburger");
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+});
+const target2 = document.getElementById("nav");
+target.addEventListener('click', () => {
+  target2.classList.toggle('open');
+});
